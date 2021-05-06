5bb1a9f55a13d6d7ce3ede4f36b7d7b7bbb6cef6
Thang Nguyen <thang.nguyen@bnksolution.com>
2021-05-06 14:31:57 +0700

    Add Post Put Delete Methods

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({
   extended: true
 }));
+app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
@@ -25,22 +26,136 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-// Get method
-app.route("/articles").get(function(req, res){
-    Article.find(function(err, articles){
+app.route("/articles")
+
+// Get All Articles
+.get((req, res) => {
+    Article.find((err, articles) => {
         if (!err) {
             if (articles) {
-                // const jsonArticles = JSON.stringify(articles);
-                // res.send(jsonArticles);
                 res.send(articles);
             } else {
-                res.send("No articles currently in wikiDB.");
+                res.send({
+                    "message": "No articles currently in wikiDB."
+                });
             }
         } else {
-            res.send(err)
+            res.send(err);
         }
         
     });
+})
+
+// Create An Article
+.post((req, res) => {
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save((err) => {
+        if (!err) {
+            res.send(newArticle);
+        } else {
+            res.send(err);
+        }
+    });
+})
+
+// Delete All Articles
+.delete((req, res) => {
+    Article.deleteMany((err) => {
+        if (!err) {
+            res.send({
+                "message": "All articles deleted!!!"
+            });
+        } else {
+            res.send(err);
+        }
+    })
+});
+
+app.route("/articles/:title")
+
+// Get An Article By Title
+.get((req, res) => {
+    Article.findOne(
+        {
+            title: req.params.title
+        }, (err, foundArticle) => {
+        if (!err) {
+            if (foundArticle) {
+                res.send(foundArticle);
+            } else {
+                res.send({
+                    "message": "No Record Found!!!"
+                });
+            }
+        } else {
+            res.send(err);
+        }
+    });
+})
+
+// Update An Article By Title (All Fields)
+.put((req, res) => {
+    Article.update(
+        {
+            title: req.params.title
+        }, 
+        {
+            title: req.body.title,
+            content: req.body.content
+        },
+        {
+            overwrite: true
+        }, (err, updatedArticle) => {
+            if (!err) {
+                res.send({
+                    "message": "Update Success!!!"
+                });
+            } else {
+                res.send(err);
+            }
+        }
+    );
+})
+
+// Update An Article By Title (Specific Fields)
+.patch((req, res) => {
+    Article.update(
+        {
+            title: req.params.title
+        }, 
+        {
+            $set: req.body
+        }, (err, updatedArticle) => {
+            if (!err) {
+                res.send({
+                    "message": "Update Success!!!"
+                });
+            } else {
+                res.send(err);
+            }
+        }
+    );
+})
+
+// Delete An Article
+.delete((req, res) => {
+    Article.deleteOne(
+        {
+            "title": req.params.title
+        }, (err) => {
+            if (!err) {
+                res.send({
+                    "message": "Delete Success!!!"
+                });
+            } else {
+                res.send(err);
+            }
+        }
+    );
 });
 
 app.listen(3000, () => {
